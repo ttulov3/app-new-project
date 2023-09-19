df2bb0248ac051b2479bffe129ddb59bf2952224
Rebeca Araujo <104568778+ReisLimaaaa@users.noreply.github.com>
2023-09-19 14:13:11 +0000

    ...

--- App/src/app.js
+++ App/src/app.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Primeiro from "./components/primeiro";
 import {Comp, Comp2, Comp3 } from './components/multiplo';
 
 export default function App(){
     return(
-        <SafeAreaView>
+        <SafeAreaView style={style.App}>
             <Primeiro/>
             <Comp/>
             <Comp2/>
             <Comp3/>
         </SafeAreaView>
     )
+    const style = StyleSheet.create ({
+        App: {
+            flexGrow: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }
+    })
 }
--- App/src/components/multiplo.js
+++ App/src/components/multiplo.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Text } from "react-native";
+import estilo from "../estilo";
 
 function Comp () {
-    return <Text>Comp inicial</Text>
+    return <Text style={estilo.fontG}>Comp inicial</Text>
 
 }
 
 function Comp2 () {
-    return <Text>Comp 2</Text>
+    return <Text style={estilo.fontG}>Comp 2</Text>
 }
 
 function Comp3 () {
-    return <Text>Comp 3</Text>
+    return <Text style={estilo.fontG}>Comp 3</Text>
 }
 
 export {Comp, Comp2, Comp3};
